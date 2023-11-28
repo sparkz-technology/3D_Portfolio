6ca8773b28a3c1c21dfbf254a0b1213fc860334c
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-11-28 23:29:28 +0530

    Add react-router-dom dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { About, Contact, Home, Projects } from './pages'
+
 const App: React.FC = () => {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
+  const routes = createBrowserRouter([
+    { path: '/', element: <Home /> },
+    { path: '/about', element: <About /> },
+    { path: '/projects', element: <Projects /> },
+    { path: '/contact', element: <Contact /> },
+  ])
+
+  return <RouterProvider router={routes} />
 }
 
 export default App
