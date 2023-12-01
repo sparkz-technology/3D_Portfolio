adeead5ed739b18c4c26a1062a798252ef583122
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-12-01 17:54:43 +0530

    Add dependencies and update Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,30 @@
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+import Loader from '../components/Loader'
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent "
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+        </Suspense>
+      </Canvas>
+    </section>
   )
 }
 
 export default Home
+
+{
+  /* <div
+  className="absolute top-28 left-0 right-0 z-10 flex
+items-center justify-center"
+></div> */
+}
